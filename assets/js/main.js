465f78c2e08588536db360f0ed10594194c4a25e
Andre <87950637+andree404@users.noreply.github.com>
2022-06-07 01:40:11 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -79,7 +79,7 @@ $(document).ready(function() {
 
   $(function() {
     typed.typed({
-      strings: ["Ismail Tasdelen.", "Security Researcher.", "Bug Bounty Hunter.", "Developer."],
+      strings: ["Andre.", "Programmer."],
       typeSpeed: 100,
       loop: true,
     });
